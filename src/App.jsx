5814eaa5670c5fa6b81b175f9d2fb623aa104841
ANDERSON NAZARENO <>
2022-07-25 00:10:34 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Teste from './Teste.jsx'
+import axios from 'axios'
+
+function App() {
+  const [data, setData] = useState([])
+  const [dataBackup, setDataBackup] = useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/todos').then(data => {
+      setData(data.data)
+      setDataBackup(data.data)
+    })
+  }, [])
+
+  const showFinalizadas = () => {
+    setData([])
+    const finalizadas = dataBackup.filter(d => d.completed)
+    setData(finalizadas)
+  }
+
+  const showNaoFinalizadas = () => {
+    setData([])
+    const finalizadas = dataBackup.filter(d => !d.completed)
+    setData(finalizadas)
+  }
+
+  const showAll = () => {
+    setData(dataBackup)
+  }
+
+  return (
+    <>
+      <h1>Lista de Tarefas</h1>
+      <button onClick={() => showAll()}>Todas</button>
+      <button onClick={() => showFinalizadas()}>Fializadas</button>
+      <button onClick={() => showNaoFinalizadas()}>Nao Finalizadas</button>
+      <table>
+        <thead>
+          <tr>
+            <th>#ID</th>
+            <th>Titulo</th>
+            <th>Status</th>
+            <th>UserID</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(el =>
+            <tr key={el.id}>
+              <td>{el.id}</td>
+              <td>{el.title}</td>
+              <td>{el.completed ? <b style={{ color: "green" }}>OK</b> :
+                <b style={{ color: "red" }}>Aberta</b>}</td>
+              <td>{el.userId}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+
+
+export default App;
---
+++ src/Teste.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Teste = () => {
+  return (
+    <div>
+      <h1>
+        TesteComponent1
+      </h1>
+
+    </div>
+  );
+}
+
+export default Teste;
